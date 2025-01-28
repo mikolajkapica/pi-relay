4782113dfe05213efa8688eeae96bb8f3760ba74
Mikołaj Kapica <m.kapica@avsystem.com>
2025-01-28 10:13:38 +0100

    Change build script

--- compile-to-pi.sh
+++ compile-to-pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-target_path=$(nix build .#packages.aarch64-linux.default --no-link --print-out-paths -L --option sandbox false)
+target_path=$(nix build --builders "ssh://eu.nixbuild.net x86_64-linux - 100 1 big-parallel,benchmark")
 nix copy .#packages.aarch64-linux.default --to ssh://pi@192.168.1.65
 echo "$target_path/bin/pi-relay"
