4fb7a5ab98e4c9ebdb2ef6e551feb2b36401cc47
mkapica <mikolajkapica7@gmail.com>
2025-01-29 02:55:33 +0100

    Clean code!

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,150 +1,185 @@
 #include <Arduino.h>
 #include <stdarg.h>
 #include <stdio.h>
-
 #include "WiFiS3.h"
-#include <SocketIOclient.h>
-#include <WebSockets.h>
 #include <WebSocketsClient.h>
-#include <WebSocketsServer.h>
-#include <WebSocketsVersion.h>
 
+// Network configuration
 #define WIFI_SSID "wroclawski-koks"
 #define WIFI_PASS "krzysinek"
+#define WEBSOCKET_HOST "192.168.1.20"
+#define WEBSOCKET_PORT 12345
+#define WEBSOCKET_PATH "/ws"
 
-#define MOTOR_AIN1 8
-#define MOTOR_AIN2 7
-#define MOTOR_BIN1 4
-#define MOTOR_BIN2 2
+// Motor pin definitions
+#define MOTOR_LEFT_PWM 3
+#define MOTOR_RIGHT_PWM 9
+#define MOTOR_LEFT_IN1 8
+#define MOTOR_LEFT_IN2 7
+#define MOTOR_RIGHT_IN1 4
+#define MOTOR_RIGHT_IN2 2
 
-WebSocketsClient webSocket;
-int l2, r2;
+// Status LED pin
+#define STATUS_LED LED_BUILTIN
 
-void setupMotors();
-void motor_config_forward();
-void motor_config_left();
-void motor_config_right();
-
-void setupMotors() {
-    pinMode(MOTOR_AIN1, OUTPUT);
-    pinMode(MOTOR_AIN2, OUTPUT);
-    pinMode(MOTOR_BIN1, OUTPUT);
-    pinMode(MOTOR_BIN2, OUTPUT);
-
-    digitalWrite(MOTOR_AIN1, LOW);
-    digitalWrite(MOTOR_AIN2, LOW);
-    digitalWrite(MOTOR_BIN1, LOW);
-    digitalWrite(MOTOR_BIN2, LOW);
-}
-
-void motor_config_forward() {
-    digitalWrite(MOTOR_AIN2, LOW); 
-    digitalWrite(MOTOR_AIN1, HIGH);
-    digitalWrite(MOTOR_BIN1, HIGH);
-    digitalWrite(MOTOR_BIN2, LOW);
-}
-
-void motor_config_left() {
-    digitalWrite(MOTOR_AIN2, LOW);
-    digitalWrite(MOTOR_AIN1, HIGH);
-    digitalWrite(MOTOR_BIN1, LOW);
-    digitalWrite(MOTOR_BIN2, HIGH);
-}
-
-void motor_config_right() {
-    digitalWrite(MOTOR_AIN2, HIGH);
-    digitalWrite(MOTOR_AIN1, LOW);
-    digitalWrite(MOTOR_BIN1, HIGH);
-    digitalWrite(MOTOR_BIN2, LOW);
-}
-
-void parseNumbers(const char *input, int *l2, int *r2) {
-    if (sscanf(input, "l2=%d, r2=%d", l2, r2) != 2) {
-        printf("Error: Input format is incorrect\n");
+class MotorController {
+private:
+    int leftPWM, rightPWM;
+    int leftIn1, leftIn2;
+    int rightIn1, rightIn2;
+    
+public:
+    MotorController(int left_pwm, int right_pwm, 
+                   int left_in1, int left_in2, 
+                   int right_in1, int right_in2) {
+        leftPWM = left_pwm;
+        rightPWM = right_pwm;
+        leftIn1 = left_in1;
+        leftIn2 = left_in2;
+        rightIn1 = right_in1;
+        rightIn2 = right_in2;
     }
+
+    void begin() {
+        pinMode(leftPWM, OUTPUT);
+        pinMode(rightPWM, OUTPUT);
+        pinMode(leftIn1, OUTPUT);
+        pinMode(leftIn2, OUTPUT);
+        pinMode(rightIn1, OUTPUT);
+        pinMode(rightIn2, OUTPUT);
+        stop();
+    }
+
+    void stop() {
+        digitalWrite(leftIn1, LOW);
+        digitalWrite(leftIn2, LOW);
+        digitalWrite(rightIn1, LOW);
+        digitalWrite(rightIn2, LOW);
+        analogWrite(leftPWM, 0);
+        analogWrite(rightPWM, 0);
+    }
+
+    void forward() {
+        digitalWrite(leftIn2, LOW);
+        digitalWrite(leftIn1, HIGH);
+        digitalWrite(rightIn1, HIGH);
+        digitalWrite(rightIn2, LOW);
+    }
+
+    void left() {
+        digitalWrite(leftIn2, LOW);
+        digitalWrite(leftIn1, HIGH);
+        digitalWrite(rightIn1, LOW);
+        digitalWrite(rightIn2, HIGH);
+    }
+
+    void right() {
+        digitalWrite(leftIn2, HIGH);
+        digitalWrite(leftIn1, LOW);
+        digitalWrite(rightIn1, HIGH);
+        digitalWrite(rightIn2, LOW);
+    }
+
+    void setPower(int leftPower, int rightPower) {
+        analogWrite(leftPWM, leftPower);
+        analogWrite(rightPWM, rightPower);
+    }
+};
+
+class NetworkManager {
+private:
+    WebSocketsClient webSocket;
+    void (*messageCallback)(int leftPower, int rightPower);
+
+public:
+    NetworkManager(void (*callback)(int, int)) {
+        messageCallback = callback;
+    }
+
+    void begin() {
+        setupWiFi();
+        setupWebSocket();
+    }
+
+    void loop() {
+        webSocket.loop();
+    }
+
+private:
+    void setupWiFi() {
+        if (WiFi.status() == WL_NO_MODULE) {
+            Serial.println("Communication with WiFi module failed!");
+            while (true);
+        }
+
+        String fv = WiFi.firmwareVersion();
+        if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+            Serial.println("Please upgrade the firmware");
+        }
+
+        Serial.println("[Wifi]: Connecting");
+        while (WiFi.status() != WL_CONNECTED) {
+            Serial.printf("[Wifi]: Attempting to connect to %s\n", WIFI_SSID);
+            WiFi.begin(WIFI_SSID, WIFI_PASS);
+            delay(1000);
+        }
+
+        Serial.print("Connected! IP: ");
+        Serial.println(WiFi.localIP());
+    }
+
+    void setupWebSocket() {
+        webSocket.begin(WEBSOCKET_HOST, WEBSOCKET_PORT, WEBSOCKET_PATH);
+        webSocket.onEvent([this](WStype_t type, uint8_t * payload, size_t length) {
+            handleWebSocketEvent(type, payload, length);
+        });
+        webSocket.setReconnectInterval(5000);
+    }
+
+    void handleWebSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+        switch (type) {
+            case WStype_DISCONNECTED:
+                Serial.println("[WSc] Disconnected!");
+                break;
+            case WStype_CONNECTED:
+                Serial.println("[WSc] Connected!");
+                webSocket.sendTXT("Connected");
+                break;
+            case WStype_TEXT: {
+                Serial.printf("[WSc] Received: %s\n", (char *)payload);
+                int l2, r2;
+                if (sscanf((char *)payload, "l2=%d, r2=%d", &l2, &r2) == 2) {
+                    messageCallback(l2, r2);
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+};
+
+// Global objects
+MotorController motors(MOTOR_LEFT_PWM, MOTOR_RIGHT_PWM,
+                      MOTOR_LEFT_IN1, MOTOR_LEFT_IN2,
+                      MOTOR_RIGHT_IN1, MOTOR_RIGHT_IN2);
+NetworkManager* network;
+
+void handleMotorCommand(int leftPower, int rightPower) {
+    motors.forward();  // Set direction
+    motors.setPower(leftPower, rightPower);
 }
-
-void webSocketEvent(int type, uint8_t *payload, size_t length) {
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.println("[WSc] Disconnected!");
-      break;
-    case WStype_CONNECTED:
-      Serial.println("[WSc] Connected!");
-      webSocket.sendTXT("Connected");
-      break;
-    case WStype_TEXT:
-      Serial.print("[WSc] get text:");
-      Serial.println((char *)payload);
-      parseNumbers((const char*)payload, &l2, &r2);
-      break;
-    case WStype_BIN:
-    case WStype_ERROR:
-    case WStype_FRAGMENT_TEXT_START:
-    case WStype_FRAGMENT_BIN_START:
-    case WStype_FRAGMENT:
-    case WStype_FRAGMENT_FIN:
-      break;
-  }
-}
-
-
 
 void setup() {
-  setupMotors();
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-
-  Serial.begin(115200);
-  while (!Serial);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.println("[SETUP] BOOT WAIT ...");
-    Serial.flush();
-    delay(1000);
-  }
-
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    while (true);
-  }
-
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  Serial.println("[Wifi]: Connecting");
-
-  int status = WL_IDLE_STATUS;
-
-  while (status != WL_CONNECTED) {
-    Serial.print("[Wifi]: Attempting to connect to SSID: ");
-    Serial.println(WIFI_SSID);
-    status = WiFi.begin(WIFI_SSID, WIFI_PASS);
-    delay(1000);
-  }
-
-  Serial.println("Connected!");
-
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  webSocket.begin("192.168.1.20", 12345, "/ws");
-
-  webSocket.onEvent(webSocketEvent);
-
-  webSocket.setReconnectInterval(5000);
+    Serial.begin(115200);
+    while (!Serial);
+    pinMode(STATUS_LED, OUTPUT);
+    
+    motors.begin();
+    network = new NetworkManager(handleMotorCommand);
+    network->begin();
 }
 
 void loop() {
-  webSocket.loop();
-  motor_config_forward();
-  analogWrite(3, l2);
-  analogWrite(9, r2);
+    network->loop();
 }
